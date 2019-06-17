9db0202612b0183d4eb1af1f6c9529cdac1a0624
Ikhtiar <ikhtiaralam@gmail.com>
2019-06-17 09:12:04 -0500

    changes

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('Mushroom Burger', FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Lamb Burger', FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Veggie Burger', FALSE);
+
+TRUNCATE TABLE burgers;
